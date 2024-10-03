77fe6a95eaf034f3636fd999cded63ed4d8d1829
Atraue Wallis <atraue@redskyeng.com>
2024-10-03 13:42:04 -0600

    refactor: replace ternary operators with cn function

--- src/routes/(auth)/dashboard/NavItem.svelte
+++ src/routes/(auth)/dashboard/NavItem.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
+
 	interface NavItemProps {
 		name: string;
 		href: string;
@@ -10,19 +12,19 @@
 
 	const styledIcon = icon?.replace(
 		'<svg',
-		`<svg class="w-4 h-4 ${isActive ? 'fill-accent-foreground' : 'fill-muted-foreground'}"`
+		`<svg class="${cn('w-4 h-4 fill-muted-foreground', { 'fill-accent-foreground': isActive })}"`
 	);
 </script>
 
-<li class="rounded-lg p-2.5 {isActive ? 'bg-primary-foreground' : ''}">
+<li class={cn('rounded-lg p-2.5', { 'bg-primary-foreground': isActive })}>
 	<a class="flex items-center gap-x-2" {href}>
 		{#if icon}
 			{@html styledIcon}
 		{/if}
 		<span
-			class="font-inter text-lg font-semibold {isActive
-				? 'text-foreground'
-				: 'text-muted-foreground'}">{name}</span
+			class={cn('font-inter text-lg font-semibold text-muted-foreground', {
+				'text-foreground': isActive
+			})}>{name}</span
 		>
 	</a>
 </li>
