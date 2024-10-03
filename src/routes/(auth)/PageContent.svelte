548e684a5f0462c8c166a50f897d6fded0b9a76a
Atraue Wallis <atraue@redskyeng.com>
2024-10-03 14:09:31 -0600

    refactor: remove DashboardContent component

--- src/routes/(auth)/PageContent.svelte
+++ src/routes/(auth)/PageContent.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = HTMLAttributes<HTMLDivElement>;
 
-	let className: $$Props["class"] = undefined;
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
-<div class={cn("flex flex-grow flex-col desktop:flex-row items-center gap-4 justify-center p-4 bg-muted-opacity", className)}>
-    <slot/>
+<div
+	class={cn(
+		'flex flex-grow flex-col items-center justify-center gap-4 bg-muted-opacity p-4 desktop:flex-row',
+		className
+	)}
+>
+	<slot />
 </div>
--- src/routes/(auth)/dashboard/+page.svelte
+++ src/routes/(auth)/dashboard/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	import DashboardContent from './DashboardContent.svelte';
+	import PageContent from '../PageContent.svelte';
+	import icon from '$lib/assets/images/li-layout-dashboard.svg';
 </script>
 
-<DashboardContent />
+<PageContent>
+	<img class="mb-4 h-24 w-24" src={icon} alt="Dashboard icon" />
+	<h2 class="mb-2 font-inter text-xl font-semibold text-foreground">Welcome to the Dashboard</h2>
+	<p class="text-center font-inter text-base font-normal text-muted-foreground">
+		Here is a brief description about the dashboard
+	</p>
+</PageContent>
