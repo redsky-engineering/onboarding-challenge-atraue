9a1338f7bafe034bde958d0310427c44a46da5f6
Atraue Wallis <atraue@redskyeng.com>
2024-10-04 10:15:06 -0600

    fix: remove break tag

--- src/routes/(auth)/weather/components/ResultsCard.svelte
+++ src/routes/(auth)/weather/components/ResultsCard.svelte
@@ -17,7 +17,7 @@
 	<Card.Header>
 		<Card.Title tag="h2">Results</Card.Title>
 		<Card.Description
-			>Introducing Our Dynamic Orders Dashboard for Seamless Management and<br/>Insightful Analysis.
+			>Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.
 		</Card.Description>
 	</Card.Header>
 	<Card.Content class="pt-0 pb-6">
