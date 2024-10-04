1a07ecbb4293c752315be682b4d09a65fe905a59
Atraue Wallis <atraue@redskyeng.com>
2024-10-04 11:05:41 -0600

    fix: match style for mobile

--- src/routes/(auth)/weather/components/ForecastTomorrow.svelte
+++ src/routes/(auth)/weather/components/ForecastTomorrow.svelte
@@ -10,7 +10,7 @@
 
 <div class={className}>
 	<h3 class="text-lg font-inter font-medium mb-4 text-card-foreground">Tomorrow's Forecast</h3>
-	<div class="grid grid-cols-[repeat(2,1fr)] desktop:grid-cols-[repeat(2, 10rem)] gap-x-2 gap-y-4">
+	<div class="grid grid-cols-[repeat(2,1fr)] desktop:grid-cols-[repeat(2,10rem)] gap-x-2 gap-y-4">
 		<ForecastItem title="High" value={highTemp} unit="° F" />
 		<ForecastItem title="Low" value={lowTemp} unit="° F" />
 	</div>
