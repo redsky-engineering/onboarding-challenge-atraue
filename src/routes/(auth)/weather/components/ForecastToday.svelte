233e6408ac69318a385db7d8861d567471f040bb
Atraue Wallis <atraue@redskyeng.com>
2024-10-04 10:47:51 -0600

    fix: make sure styles match for mobile and desktop

--- src/routes/(auth)/weather/components/ForecastToday.svelte
+++ src/routes/(auth)/weather/components/ForecastToday.svelte
@@ -15,12 +15,12 @@
 <div class={className}>
 	<h3 class="font-inter text-lg font-medium text-card-foreground mb-4">Today's Forecast</h3>
 	<div class="grid grid-cols-[repeat(3,1fr)] desktop:grid-cols-[repeat(3,10rem)] gap-x-2 gap-y-4">
-		<ForecastItem title="Current Temperature" value={currentTemp} unit="° F" />
+		<ForecastItem class="col-span-2" title="Current Temperature" value={currentTemp} unit="° F" />
 		<div></div>
-		<div></div>
-		<ForecastItem title="Sunrise" value={sunrise} unit="am" />
-		<ForecastItem title="Sunset" value={sunset} unit="pm" />
-		<div></div>
+		<div class="col-span-3 flex gap-x-2 desktop:contents">
+		<ForecastItem class="w-[50%] desktop:w-fit" title="Sunrise" value={sunrise} unit="am" />
+		<ForecastItem class="w-[50%] desktop:w-fit desktop:col-span-2" title="Sunset" value={sunset} unit="pm" />
+		</div>
 		<ForecastItem title="Pressure" value={pressure} unit="hPa" />
 		<ForecastItem title="Humidity" value={humidity} unit="%" />
 		<ForecastItem title="Wind" value={wind} unit="mph" />
